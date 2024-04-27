36fe5cdeaba527eee4335b0a2b9da1b96e54257a
janithanupama <166873374+janithanupama@users.noreply.github.com>
2024-04-27 09:13:45 +0200

    Update create_model.py

--- Transfer_VGG16/create_model.py
+++ Transfer_VGG16/create_model.py
@@ -1,37 +1,4 @@
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.utils import plot_model
-# from tensorflow.keras.layers import TimeDistributed
-# from tensorflow.keras.layers import LSTM,Dense,Conv2D
-# from tensorflow.keras.layers import Dropout,MaxPooling2D,Flatten
-#
-# def create_model(SEQUENCE_LENGTH,IMAGE_HEIGHT,IMAGE_WIDTH):
-#     model = Sequential()
-#     model.add(TimeDistributed(Conv2D(16, (3, 3), padding='same',activation = 'relu'),
-#                               input_shape = (SEQUENCE_LENGTH, IMAGE_HEIGHT, IMAGE_WIDTH,1)))
-#
-#     model.add(TimeDistributed(MaxPooling2D((4, 4))))
-#     model.add(TimeDistributed(Dropout(0.5)))
-#
-#     model.add(TimeDistributed(Conv2D(32, (3, 3), padding='same',activation = 'relu')))
-#     model.add(TimeDistributed(MaxPooling2D((4, 4))))
-#     model.add(TimeDistributed(Dropout(0.5)))
-#
-#     model.add(TimeDistributed(Conv2D(64, (3, 3), padding='same',activation = 'relu')))
-#     model.add(TimeDistributed(MaxPooling2D((2, 2))))
-#     model.add(TimeDistributed(Dropout(0.25)))
-#
-#     model.add(TimeDistributed(Conv2D(64, (3, 3), padding='same',activation = 'relu')))
-#     model.add(TimeDistributed(MaxPooling2D((2, 2))))
-#
-#     model.add(TimeDistributed(Flatten()))
-#
-#     model.add(LSTM(32))
-#
-#     model.add(Dense(2, activation = 'softmax'))
-#     model.summary()
-#     return model
-
-# #%%
+#%%
 from keras.applications.vgg16 import VGG16
 from keras.models import Model
 from keras.layers import Dense, Input
