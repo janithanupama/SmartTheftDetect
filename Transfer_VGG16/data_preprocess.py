42d2a48f31654331dc6b1a9da09c8df8f0c7f2ed
janithanupama <166873374+janithanupama@users.noreply.github.com>
2024-04-27 09:13:16 +0200

    Update data_preprocess.py

--- Transfer_VGG16/data_preprocess.py
+++ Transfer_VGG16/data_preprocess.py
@@ -12,7 +12,7 @@ def frames_extraction(video_path):
     frame_list = []
     video_reader = cv2.VideoCapture(video_path)
 
-    video_frames_count = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT)) #--> Toatl Number of Frames
+    video_frames_count = int(video_reader.get(cv2.CAP_PROP_FRAME_COUNT)) #--> Total Number of Frames
     skip_frames_window = max(int(video_frames_count/SEQUENCE_LENGTH),1)  # skip Frequency
 
     for frame_counter in range(SEQUENCE_LENGTH):
@@ -24,28 +24,29 @@ def frames_extraction(video_path):
 
         if not success:
             break
-        #hi janith
         # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # blurred = cv2.GaussianBlur(gray, (5, 5), 21)
         # (T, threshInv) = cv2.threshold(blurred, 0, 255,cv2.THRESH_BINARY| cv2.THRESH_OTSU)
-        #hi janith
         # img = cv2.medianBlur(gray,5)
         # img = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,7,3)
-        #Hey
+
         resized_frame = cv2.resize(frame,(IMAGE_WIDTH,IMAGE_HEIGHT))
+        
         # cv2.imshow("LIVE",resized_frame)
         # cv2.waitKey(0)
+        
         normalized_frame = resized_frame/255
         frame_list.append(normalized_frame)
 
     video_reader.release()
     frame_list = np.array(frame_list)
+    
     # cv2.destroyAllWindows()
+    
     return frame_list
 
 # set the path of the image folder
-video_folder = r'D:\fyp\FYP VIDEOS EDIT\Thef_Detection\videos'
-#video_folder = r'D:\fyp\FYP VIDEOS EDIT\videos'
+video_folder = r'D:\Thef_Detection\videos' # Path to Videos
 categories = os.listdir(video_folder)
 
 data = []
